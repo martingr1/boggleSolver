012b0de954e851256630daeddf98d37c93a7a136
martingr1 <martingarryrussell@gmail.com>
2019-10-08 21:34:25 +0000

    added further lesson code

--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,6 @@
 import unittest
 import boggle
+from string import ascii_uppercase
 
 
 class TestBoggle(unittest.TestCase):
@@ -11,26 +12,58 @@ class TestBoggle(unittest.TestCase):
         """
         Test to see if we can create an empty grid
         """
+        grid = boggle.make_grid(0, 0)
+        self.assertEqual(len(grid), 0)
 
-        grid = boggle.make_grid(0,0)
-        self.assertEqual(len(grid),  0)
-
-    def test_grid_size_is_width_times_height(self):
+    def test_grid_size_width_times_height(self):
         """
-        Test is to ensure total size of grid
+        Test is to ensure that the total size of the grid
         is equal to width * height
         """
         grid = boggle.make_grid(2, 3)
         self.assertEqual(len(grid), 6)
 
     def test_grid_coordinates(self):
+        grid = boggle.make_grid(2, 2)
+        self.assertIn((0, 0), grid)
+        self.assertIn((0, 1), grid)
+        self.assertIn((1, 0), grid)
+        self.assertIn((1, 1), grid)
+        self.assertNotIn((2, 2), grid)
+
+    def test_grid_is_filled_with_letters(self):
         """
-        Test to ensure all of the coords
-        inside grid can be accessed
+        Ensure that each of the coordinates in the grid
+        contains letters
         """
-        grid = boggle.make_grid(2,2)
-        self.assertIn((0,0), grid)
-        self.assertIn((0,1), grid)
-        self.assertIn((1,0), grid)
-        self.assertIn((1,1), grid)
-        self.assertNotIn((2,2), grid)
+        grid = boggle.make_grid(2, 3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+
+    def test_neighbours_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbours
+        """
+        coords = (1, 2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0, 1), neighbours)
+        self.assertIn((0, 2), neighbours)
+        self.assertIn((0, 3), neighbours)
+        self.assertIn((1, 1), neighbours)
+        self.assertIn((1, 3), neighbours)
+        self.assertIn((2, 1), neighbours)
+        self.assertIn((2, 2), neighbours)
+        self.assertIn((2, 3), neighbours)
+
+
+    def test_all_grid_neighbours(self):
+        """
+        Ensure that all of the grid positions have neighbours
+        """
+        grid = boggle.make_grid(2, 2)
+        neighbours = boggle.all_grid_neighbours(grid)
+        self.assertEqual(len(neighbours), len(grid))
+        for pos in grid:
+            others = list(grid) # create a new list from the dictionary's keys
+            others.remove(pos)
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others))
